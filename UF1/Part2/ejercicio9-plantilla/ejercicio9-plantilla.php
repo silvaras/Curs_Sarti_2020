407f9e9d5855fe452e109e6e4809438f54ec646f
Toni Ruiz <toniruizc@gmail.com>
2020-02-04 13:28:18 +0100

    Ejercicio9 plantilla

---
+++ UF1/Part2/ejercicio9-plantilla/ejercicio9-plantilla.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    		<meta charset="utf-8" />
+    		<title>Page Title</title>
+    		<link rel="stylesheet" type="text/css" href="css/Ejercicio9.css" />
+ 	</head>
+
+	<body>
+		<div id="cabecera">
+			<h1>Titol inicial</h1>
+		</div>
+
+		<div id="menu">
+			<ul>
+	    		<li><a href="#">Opció 1</a></li>
+	    		<li><a href="#">Opció 2</a></li>
+	    		<li><a href="#">Opció 3</a></li>
+	    		<li><a href="#">Inici</a></li>
+
+    		</ul>
+		</div>
+
+		<div id="contingut">
+            <p>Contingut de la pàgina web</p>
+		</div>
+
+		<div id="pie">
+			<p>Peu de la pàgina web</p>
+		</div>
+	</body>
+</html>
