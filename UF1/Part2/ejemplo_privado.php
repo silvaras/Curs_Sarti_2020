727618257c4c41df77f6ada149b622c5406d56d2
Toni Ruiz <toniruizc@gmail.com>
2020-02-11 00:03:23 +0100

    Exemple login i pagina privada

---
+++ UF1/Part2/ejemplo_privado.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if(isset($_REQUEST["logout"])){
+    $_SESSION=null;
+    session_destroy(); 
+    setcookie("password",0,1);
+    setcookie("nomusuari",0,1);
+
+    header('Location:ejemplo_login.php');           
+
+}
+
+
+
+
+if(isset($_SESSION["login"])&&$_SESSION["login"]==true){
+?>
+Bienvenido......<?=$_SESSION["nom"]?>
+
+<a href="ejemplo_privado.php?logout">[logout]</a>
+<?php
+}else{
+    header('Location:ejemplo_login.php');           
+
+}
+?>
